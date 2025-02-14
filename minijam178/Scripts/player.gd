0b676cce34d1d6c65a3f37fbc1e43ee36c551499
Kbloody <elchollodeambos@gmail.com>
2025-02-14 20:20:41 +0100

    Nombres cambiados y tal ytal

--- minijam178/Scripts/player.gd
+++ minijam178/Scripts/player.gd
@@ -1,47 +1,14 @@
 extends CharacterBody2D
 
-@export var speed := 150.0
-@export var sprint_multiplier := 1.5
-@export var jump_force := 250.0
-@export var gravity := 500.0
+@export var SPEED = 300.0
 
-var velocity_y := 0.0  # Velocidad vertical (para el salto)
-var is_grounded := true
 
-func _physics_process(delta):
-	var direction := Vector2.ZERO
-	if Input.is_action_pressed("ui_right"):
-		direction.x += 1
-	if Input.is_action_pressed("ui_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		direction.y += 1
-	if Input.is_action_pressed("ui_up"):
-		direction.y -= 1
-
-	if direction != Vector2.ZERO:
+func _physics_process(_delta):	
+	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
+	
+	if(direction.length_squared() > 1):
 		direction = direction.normalized()
 
-	var current_speed = speed
-	if Input.is_action_pressed("ui_shift"):
-		current_speed *= sprint_multiplier
-
-	velocity.x = direction.x * current_speed
-	velocity.y = direction.y * current_speed
-
-	# Manejo del salto
-	if is_grounded and Input.is_action_just_pressed("ui_accept"):
-		velocity_y = -jump_force
-		is_grounded = false
-
-	# Aplicar gravedad
-	if not is_grounded:
-		velocity_y += gravity * delta
-
-	# Detectar si aterriza
-	if position.y >= 0:  # Suelo en Y = 0
-		position.y = 0
-		velocity_y = 0
-		is_grounded = true
-
+	velocity = direction * SPEED
+	
 	move_and_slide()
