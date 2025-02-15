6c14ac6b528f0bc3937c9879ab0c9320eb697d0e
Kbloody <elchollodeambos@gmail.com>
2025-02-15 16:17:18 +0100

    serebro funsiona

--- minijam178/Scripts/brain.gd
+++ minijam178/Scripts/brain.gd
@@ -1,4 +1,19 @@
-extends Node2D
+extends StaticBody2D
+@onready var brain_timer: Timer = $brainTimer
+@onready var thought_spawner: Node2D = $thoughtSpawner
+@onready var rage_bar: TextureProgressBar = $rage_bar
+
+
+const BRAINTHOUGHTS = preload("res://Scenes/brainthoughts.tscn")
+
+
+
+@export_group("rage") #all var related to the rage of the monster
+@export var max_rage:float = 100
+@export var rage_change:float = 1
+@export var rage:float = 0
+
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,4 +23,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	rage_bar.value = rage
+	
+	if thought_spawner.get_child_count() != 0 and rage <= 100:
+		rage += rage_change
+	elif rage >= 0:
+		rage -= rage_change
+
+func spawnquestion():
+	var question = BRAINTHOUGHTS.instantiate()
+	question.position = Vector2(0,0)
+	thought_spawner.add_child(question)
+
+
+func _on_brain_timer_timeout() -> void:
+	if thought_spawner.get_child_count() <= 0:
+		spawnquestion()
+		print("question spawned")
---
+++ minijam178/Scripts/brainthoughts.gd
@@ -0,0 +1,78 @@
+extends Node2D
+
+
+@onready var question: Label = $Container/Question
+@onready var answer_1: Label = $Container/Answer1/Answer1
+@onready var answer_2: Label = $Container/Answer2/Answer2
+@onready var answer_3: Label = $Container/Answer3/Answer3
+
+var operators = ["+", "-", "*"]
+var valuerandomness: int = 20
+var rightanswer
+
+func generate_operation():
+	
+	var operator = operators[randi() % operators.size()]
+	var num1 = randi() % valuerandomness * GlobalVariables.dificulty
+	var num2 = randi() % valuerandomness * GlobalVariables.dificulty
+
+	var problem = str(num1)+operator+str(num2)
+	var expression = Expression.new()
+	var error = expression.parse(problem)
+	
+	
+	
+	
+	if error != OK:
+		push_error("error to solve" + problem)
+		return null
+
+	var answer = expression.execute()
+	var wronganswer1 = answer + randi_range(-10,25)
+	var wronganswer2 = answer + wronganswer1 + randi_range(-10,25)
+	while wronganswer1 == answer:
+		wronganswer1 = answer + randi_range(-10, 25)
+	while wronganswer2 == answer or wronganswer2 == wronganswer1:
+		wronganswer2 = answer + randi_range(-10, 25)
+	
+	return {
+		"problem": problem,
+		"answers": [answer, wronganswer1, wronganswer2]
+	}
+
+
+func _ready() -> void:
+	var operation = generate_operation()
+	rightanswer = operation["answers"][0]
+	
+	
+	var shuffled_answers = operation["answers"]
+	shuffled_answers.shuffle()
+	
+	question.text = str(operation["problem"])
+	answer_1.text = str(shuffled_answers[0])
+	answer_2.text = str(shuffled_answers[1])
+	answer_3.text = str(shuffled_answers[2])
+
+
+func _on_answer_1_area_entered(area: Area2D) -> void:
+	if area.is_in_group("punch"):
+		if answer_1.text == str(rightanswer):
+			print("right answer")
+			queue_free()
+		else:
+			print("wrong answer")
+func _on_answer_2_area_entered(area: Area2D) -> void:
+	if area.is_in_group("punch"):
+		if answer_2.text == str(rightanswer):
+			print("right answer")
+			queue_free()
+		else:
+			print("wrong answer")
+func _on_answer_3_area_entered(area: Area2D) -> void:
+	if area.is_in_group("punch"):
+		if answer_3.text == str(rightanswer):
+			print("right answer")
+			queue_free()
+		else:
+			print("wrong answer")
