8d9e844e20c7283bb2577f2977e77f8d324cbfb8
Wendfrey <toni.cano.redondo@gmail.com>
2025-02-16 23:38:20 +0100

    tutorial expanded, added fixes for gamepad control

--- minijam178/Scenes/lose.gd
+++ minijam178/Scenes/lose.gd
@@ -1,11 +1,13 @@
 extends Control
 
+@onready var button: Button = $Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	MusicPlayer.stream = load("res://Sounds/game-over-284367.mp3")
 	MusicPlayer.volume_db = -5.7
 	MusicPlayer.play()
+	button.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- minijam178/Scenes/win.gd
+++ minijam178/Scenes/win.gd
@@ -1,9 +1,12 @@
 extends Control
 
+@onready var button: Button = $MarginContainer2/Button
+
 func _ready():
 	MusicPlayer.stream = load("res://Sounds/congratulations-deep-voice-172193.mp3")
 	MusicPlayer.volume_db = 2
 	MusicPlayer.play()
+	button.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
